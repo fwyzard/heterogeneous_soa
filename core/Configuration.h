2beda2d17dc7b5081b33785d047805f6198ac283
Andrea Bocci <andrea.bocci@cern.ch>
2022-04-09 21:09:39 +0200

    Add partial error checking

--- core/Configuration.h
+++ core/Configuration.h
@@ -9,6 +9,10 @@ using Configuration = std::unordered_map<std::string, std::any>;
 
 template <typename T>
 T get(Configuration const& configuration, std::string const& parameter) {
-  // add checks here
+  if (configuration.find(parameter) == configuration.end()) {
+    std::cerr << "The requested configuration parameter of type " << typeid(T).name() << " and label '" << parameter << "' is not available" << std::endl;
+    abort();
+  }
+  // add here an explicit check on the parameter type
   return std::any_cast<T>(configuration.at(parameter));
 }
--- core/Data.h
+++ core/Data.h
@@ -15,7 +15,12 @@ public:
 
   template <typename T>
   T const& get(std::string const& label) const {
+    if (data_.find(label) == data_.end()) {
+      std::cerr << "The requested collection of type " << typeid(T).name() << " and label '" << label << "' is not available" << std::endl;
+      abort();
+    }
     WrapperBase const& wb = *data_.at(label);
+    // add here an explicit check on the collection type
     auto const& w = dynamic_cast<Wrapper<T> const&>(wb);
     return w.get();
   }
