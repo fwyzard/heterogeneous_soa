0565e6fdab04534b57204dd4f1c430b8bcd86dca
Andrea Bocci <andrea.bocci@cern.ch>
2022-04-12 08:12:13 +0200

    Improve error detection and reporting

--- src/core/Data.h
+++ src/core/Data.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <unordered_map>
 
+#include <boost/core/demangle.hpp>
+
 #include "core/Wrapper.h"
 
 class Data {
@@ -16,14 +18,21 @@ public:
   template <typename T>
   T const& get(std::string const& label) const {
     if (data_.find(label) == data_.end()) {
-      std::cerr << "The requested collection of type " << typeid(T).name() << " and label '" << label
-                << "' is not available" << std::endl;
+      std::cerr << "The requested collection with label '" << label << "' and type '"
+                << boost::core::demangle(typeid(T).name()) << "' is not available" << std::endl;
       abort();
     }
     WrapperBase const& wb = *data_.at(label);
-    // add here an explicit check on the collection type
-    auto const& w = dynamic_cast<Wrapper<T> const&>(wb);
-    return w.get();
+    if (typeid(T) == wb.type()) {
+      auto const& w = dynamic_cast<Wrapper<T> const&>(wb);
+      return w.get();
+    } else {
+      std::cerr << "The requested collection with label '" << label << "' and type '"
+                << boost::core::demangle(typeid(T).name()) << "' is not available" << std::endl;
+      std::cerr << "The collection with label '" << label << "' is of type '" << boost::core::demangle(wb.type().name())
+                << "'" << std::endl;
+      abort();
+    }
   }
 
   void clear() { data_.clear(); }
--- src/core/Wrapper.h
+++ src/core/Wrapper.h
@@ -1,23 +1,26 @@
 #pragma once
 
+#include <typeinfo>
+
 class WrapperBase {
 public:
-  WrapperBase() = default;
+  template <typename T>
+  WrapperBase(T const&) : type_{&typeid(T)} {}
+
   virtual ~WrapperBase() = default;
+
+  std::type_info const& type() const { return *type_; }
+
+private:
+  std::type_info const* type_;
 };
 
 template <typename T>
 class Wrapper : public WrapperBase {
 public:
-  //Wrapper() = default;
+  Wrapper(T const& product) : WrapperBase{product_}, product_{product} {}
 
-  Wrapper(T const& product) : product_{product} {}
-
-  Wrapper(T&& product) : product_{std::move(product)} {}
-
-  //void set(T const& product) { product_ = product; }
-
-  //void set(T&& product) { product_ = std::move(product); }
+  Wrapper(T&& product) : WrapperBase{product_}, product_{std::move(product)} {}
 
   T const& get() const { return product_; }
 
